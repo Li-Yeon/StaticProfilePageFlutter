454db3c527e00babd23361f5d537ed06ad0013fa
Li Yeon <mekaleon27@gmail.com>
2022-02-19 18:03:50 +0800

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(
+  home: ProfileCard(),
+));
+
+class ProfileCard extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
